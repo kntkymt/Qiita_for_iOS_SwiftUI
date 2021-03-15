9e5ee1fbd500d8a5c62a90304aa86bc3b6612454
kntkymt <kntkc0de@gmail.com>
2021-03-15 15:22:52 +0900

    initial プロジェクト作成

---
+++ Qiita_SwiftUI/Qiita_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qiita_SwiftUIApp.swift
+//  Qiita_SwiftUI
+//
+//  Created by kntk on 2021/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Qiita_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
