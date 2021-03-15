d3bff66e91760fef5a350a9a7ca08519781fdca9
kntkymt <kntkc0de@gmail.com>
2021-03-15 22:32:40 +0900

    [Add] Loggerセットアップ

--- Qiita_SwiftUI/Qiita_SwiftUIApp.swift
+++ Qiita_SwiftUI/Qiita_SwiftUIApp.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 @main
 struct Qiita_SwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             LaunchView(authRepository: AuthStubService())
                 .environmentObject(AuthState(authRepository: AuthStubService()))
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        setupLogger()
+
+        return true
+    }
+
+    // MARK: - Private
+
+    private func setupLogger() {
+        Logger.setup()
+        Logger.info("💫 Application will finish launching")
     }
 }
