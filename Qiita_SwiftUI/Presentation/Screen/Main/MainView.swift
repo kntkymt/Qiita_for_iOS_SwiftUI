58813dde9d05b7fe84e80161bc510d4e41e08920
kntkymt <kntkc0de@gmail.com>
2021-03-15 22:31:35 +0900

    [Add] MainViewを仮置き

--- Qiita_SwiftUI/Presentation/Screen/Main/MainView.swift
+++ Qiita_SwiftUI/Presentation/Screen/Main/MainView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+            Text("Stock")
+                .tabItem {
+                    Image(systemName: "folder")
+                    Text("Stock")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+        }
     }
 }
 
