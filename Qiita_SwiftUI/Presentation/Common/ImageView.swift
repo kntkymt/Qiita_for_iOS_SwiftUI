65b9b52d265b81212944dd809d9488c46633a2dc
kntkymt <kntkc0de@gmail.com>
2021-05-15 05:27:36 +0900

    [Add] ImageView urlから画像を表示するView

---
+++ Qiita_SwiftUI/Presentation/Common/ImageView.swift
@@ -0,0 +1,28 @@
+//
+//  ImageView.swift
+//  Qiita_SwiftUI
+//
+//  Created by kntk on 2021/05/15.
+//
+
+import SwiftUI
+import FetchImage
+
+struct ImageView: View {
+    let url: URL
+
+    @StateObject private var image = FetchImage()
+
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.gray)
+            image.view?
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .clipped()
+        }
+        .onAppear { image.load(url) }
+        .onChange(of: url) { image.load($0) }
+        .onDisappear(perform: image.reset)
+    }
+}
