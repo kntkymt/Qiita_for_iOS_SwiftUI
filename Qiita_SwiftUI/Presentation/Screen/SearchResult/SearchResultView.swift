a16138f72f8f7e5cf98b584a3a7fbc55b3230ca9
kntkymt <kntkc0de@gmail.com>
2021-09-13 02:44:41 +0900

    [Fix] ButtonにするとなぜかHeader全体に当たり判定が出るのでText.onTapGestureに

--- Qiita_SwiftUI/Presentation/Screen/SearchResult/SearchResultView.swift
+++ Qiita_SwiftUI/Presentation/Screen/SearchResult/SearchResultView.swift
@@ -14,15 +14,17 @@ struct SearchResultView: View {
     @ObservedObject private var viewModel: SearchResultViewModel
     private let likeRepository: LikeRepository
     private let stockRepository: StockRepository
+    private let tagRepository: TagRepository
 
     @State private var isInitialOnAppear = true
 
     // MARK: - Initializer
 
-    init(searchType: SearchType, itemRepository: ItemRepository, likeRepository: LikeRepository, stockRepository: StockRepository) {
+    init(searchType: SearchType, itemRepository: ItemRepository, likeRepository: LikeRepository, stockRepository: StockRepository, tagRepository: TagRepository) {
         viewModel = SearchResultViewModel(searchType: searchType, itemRepository: itemRepository)
         self.likeRepository = likeRepository
         self.stockRepository = stockRepository
+        self.tagRepository = tagRepository
     }
 
     // MARK: - Body
@@ -33,7 +35,7 @@ struct SearchResultView: View {
         GeometryReader { reader in
             ItemListView(items: $viewModel.items, isRefreshing: $viewModel.isRefreshing, onItemStockChangedHandler: nil, likeRepository: likeRepository, stockRepository: stockRepository, onRefresh: viewModel.fetchItems, onPaging: viewModel.fetchMoreItems, header: {
                 if case .tag(let tag) = viewModel.searchType {
-                    TagInformationView(tag: tag)
+                    TagInformationView(tag: tag, tagRepository: tagRepository)
                         .frame(width: reader.size.width - 32)
                 }
             })
@@ -59,21 +61,53 @@ struct TagInformationView: View {
 
     // MARK: - Property
 
-    var tag: ItemTag
+    @ObservedObject private var viewModel: TagInformationViewModel
+    @State private var isInitialOnAppear = true
+
+    // MARK: - Initializer
+
+    init(tag: ItemTag, tagRepository: TagRepository) {
+        self.viewModel = TagInformationViewModel(tag: tag, tagRepository: tagRepository)
+
+        /// FIXME: ItemListItemと同様にonAppearでやると再描画されて状態が上書きされる
+        viewModel.checkIsFollowed()
+    }
 
     // MARK: - Body
 
     var body: some View {
         VStack(alignment: .center, spacing: 8) {
-            ImageView(url: tag.iconUrl!)
+            ImageView(url: viewModel.tag.iconUrl!)
                 .frame(width: 150, height: 150)
 
-            Text(tag.id)
+            Text(viewModel.tag.id)
                 .font(.system(size: 20, weight: .medium))
 
             HStack(spacing: 32) {
-                ContributionView(title: "記事", count: tag.itemsCount)
-                ContributionView(title: "フォロワー", count: tag.followersCount)
+                ContributionView(title: "記事", count: viewModel.tag.itemsCount)
+                ContributionView(title: "フォロワー", count: viewModel.tag.followersCount)
+            }
+
+            if viewModel.isFollowed {
+                Text("フォロー中")
+                    .font(.system(size: 18, weight: .bold))
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .border(Color("brand"), width: 1)
+                    .foregroundColor(Color.white)
+                    .background(Color("brand"))
+                    .frame(width: 150, height: 30)
+                    .onTapGesture { viewModel.unfollow() }
+            } else {
+                Text("フォローする")
+                    .font(.system(size: 18, weight: .bold))
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .border(Color("brand"), width: 1)
+                    .foregroundColor(Color("brand"))
+                    .background(Color.clear)
+                    .frame(width: 150, height: 30)
+                    .onTapGesture { viewModel.follow() }
             }
         }
     }
@@ -81,8 +115,8 @@ struct TagInformationView: View {
 
 struct SearchResultView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchResultView(searchType: .word("iOS"), itemRepository: ItemStubService(), likeRepository: LikeStubService(), stockRepository: StockStubService())
+        SearchResultView(searchType: .word("iOS"), itemRepository: ItemStubService(), likeRepository: LikeStubService(), stockRepository: StockStubService(), tagRepository: TagStubService())
 
-        SearchResultView(searchType: .tag(ItemTag(iconUrl: URL(string: "https://avatars2.githubusercontent.com/u/44288050?v=4")!, followersCount: 10, id: "iOS", itemsCount: 10)), itemRepository: ItemStubService(), likeRepository: LikeStubService(), stockRepository: StockStubService())
+        SearchResultView(searchType: .tag(ItemTag(iconUrl: URL(string: "https://avatars2.githubusercontent.com/u/44288050?v=4")!, followersCount: 10, id: "iOS", itemsCount: 10)), itemRepository: ItemStubService(), likeRepository: LikeStubService(), stockRepository: StockStubService(), tagRepository: TagStubService())
     }
 }
