7aa12a8dc9c7d16db73f44342d9dfa662d24105f
kntkymt <kntkc0de@gmail.com>
2021-10-21 18:27:13 +0900

    [Fix] LoggerPluginでの表示内容を見やすくする

--- Qiita_SwiftUI/Package/Sources/Network/LoggerPlugin.swift
+++ Qiita_SwiftUI/Package/Sources/Network/LoggerPlugin.swift
@@ -11,10 +11,16 @@ import Common
 struct LoggerPlugin: PluginType {
 
     func willSend(_ request: RequestType, target: TargetType) {
-        request.request.map { Logger.debug($0.curlString) }
+        request.request.map { Logger.debug("Target: \(target)\n\($0.curlString)") }
     }
 
     func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
-        Logger.debug(result)
+        switch result {
+        case .success(let response):
+            Logger.debug("Success\nTarget: \(target)\nResponse: \(response)")
+
+        case .failure(let error):
+            Logger.debug("Failure\nTarget: \(target)\nError: \(error)")
+        }
     }
 }
